4a6759aa20b37cac72d99636dd959abd1d9ccd9c
hamisirizwan <hamisirizwan5@gmail.com>
2025-04-07 22:24:32 +0300

    completed the rock paper scissors game

---
+++ main.js
@@ -0,0 +1,57 @@
+// const rockButton = document.getElementById("rock")
+
+// rockButton.addEventListener("click", function(){
+//     alert("You clicked rock")
+// })
+
+const resultEl = document.getElementById("result");
+const userOptionEl = document.getElementById("userOption");
+const computerOptionEl = document.getElementById("computerOption");
+
+const options = ["rock", "paper", "scissors"];
+
+const score = {
+    user:0,
+    computer:0
+}
+
+function playGame(userSelected) {
+  // get what the user has selected - done
+
+  userOptionEl.innerHTML = userSelected
+  // generate the computer selection
+  const randomIndex = Math.floor(Math.random() * 3)
+  const computerSelection = options[randomIndex];
+
+  computerOptionEl.innerHTML = computerSelection
+  // write conditionals to check who has won
+
+  if (userSelected === "rock" && computerSelection === "paper") {
+    score.computer += 1
+    resultEl.innerHTML = "the computer has won";
+  } else if (userSelected === "paper" && computerSelection === "rock") {
+    score.user += 1
+    resultEl.innerHTML = "the user has won";
+  } else if (userSelected === "scissors" && computerSelection === "paper") {
+    score.user += 1
+    resultEl.innerHTML = "the user has won";
+  } else if (userSelected === "paper" && computerSelection === "scissors") {
+    score.computer += 1
+    resultEl.innerHTML = "the computer has won";
+  } else if (userSelected === "rock" && computerSelection === "scissors") {
+    score.user += 1
+    resultEl.innerHTML = "the user has won";
+  } else if (userSelected === "scissors" && computerSelection === "rock") {
+    score.computer += 1
+    resultEl.innerHTML = "the computer has won";
+  } else if (userSelected === computerSelection) {
+    resultEl.innerHTML = "it's a draw";
+  } else {
+    resultEl.innerHTML = "invalid input";
+  }
+
+
+  document.getElementById("computerScore").innerHTML = score.computer
+  document.getElementById("userScore").innerHTML = score.user
+//   console.log(score)
+}
---
+++ math.js
@@ -0,0 +1,28 @@
+// the math object provides a set of methods that allow us to perform mathematical operations
+
+// console.log(Math.PI)
+
+console.log(Math.pow(3 , 2))
+
+// rounding off numbers
+
+// 5.6 = 6
+//  5.4 = 5
+
+const rounderNumber = Math.round(5.4)
+
+// console.log(rounderNumber)
+
+const rounderDownNumber = Math.floor(5.6)
+
+console.log(rounderDownNumber)
+
+const rounderUpNumber = Math.ceil(5.1)
+
+console.log(rounderUpNumber)
+
+const randomNumber = Math.random() * 3
+
+console.log(Math.floor(randomNumber))
+
+// const randomHexValue = Math.random() * 3
